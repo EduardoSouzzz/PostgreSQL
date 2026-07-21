c6189776e901a85cd67a32f42edc51da424b8036
Eduardo Souza <dudujv1910@gmail.com>
2026-07-21 17:09:40 -0300

    Add foreign key relationship for cliente and nacionalidade
    Establish foreign key relationship between cliente and nacionalidade tables by adding idnacionalidade column and updating records.

---
+++ Relacionamento Foreign Key/Tabela cliente e nacionalidade.sql
@@ -0,0 +1,10 @@
+-- RELACIONAMENTO COM CLIENTE E NACIONALIDADE;;
+
+select * from cliente;
+alter table cliente drop nacionalidade;
+alter table cliente add idnacionalidade integer;
+alter table cliente add constraint fk_cln_idnacionalidade foreign key (idnacionalidade) references nacionalidade (idnacionalidade);
+update cliente set idnacionalidade = 1 where idcliente in (1, 2, 3, 4, 6, 10, 11, 14);
+update cliente set idnacionalidade = 2 where idcliente in (5, 7);
+update cliente set idnacionalidade = 3 where idcliente = 8;
+update cliente set idnacionalidade = 4 where idcliente in (9,13);
