442b4bad798e113191dfb07c5bbad1f3392049ce
Eduardo Souza <dudujv1910@gmail.com>
2026-07-21 17:10:47 -0300

    Add 'uf' table for state information
    Create table 'uf' with primary key and unique constraints.

---
+++ Relacionamento Foreign Key/Tables cliente, municipio e uf.sql
@@ -0,0 +1,10 @@
+-- RELACIONAMENTO ENTRE CLIENTE, MUNICIPIO E UF;
+create table uf (
+    iduf integer not null,
+	nome varchar(30) not null,
+	sigla char(2) not null,
+
+	constraint pk_ufd_idunidade_federacao primary key (iduf), 
+	constraint un_ufd_nome unique (nome),
+	constraint un_ufd_sigla unique (sigla)
+);
