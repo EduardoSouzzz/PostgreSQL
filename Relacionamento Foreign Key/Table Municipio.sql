1334b23a19e04286cc7689f64e10d8e46bd302e7
Eduardo Souza <dudujv1910@gmail.com>
2026-07-21 17:11:22 -0300

    Create municipio table and update cliente relationships
    Created the municipio table with foreign key relationship to uf. Updated cliente table to reference municipio and set idmunicipio for existing records.

---
+++ Relacionamento Foreign Key/Table Municipio.sql
@@ -0,0 +1,40 @@
+-- TABLE MUNICIPIO
+
+create table municipio (
+    idmunicipio integer not null,
+	nome varchar(30) not null,
+	iduf integer not null,
+
+	constraint pk_mnc_idmunicipio primary key (idmunicipio),
+	constraint un_mnc_nome unique (nome),
+	constraint fk_mnc_iduf foreign key (iduf) references uf (iduf)
+);
+
+insert into municipio (idmunicipio, nome, iduf) values (1, 'Porto União', 1);
+insert into municipio (idmunicipio, nome, iduf) values (2, 'Canoinhas', 1);
+insert into municipio (idmunicipio, nome, iduf) values (3, 'Porto Vitória', 2);
+insert into municipio (idmunicipio, nome, iduf) values (4, 'General Carneiro', 2);
+insert into municipio (idmunicipio, nome, iduf) values (5, 'São Paulo', 3);
+insert into municipio (idmunicipio, nome, iduf) values (6, 'Rio de Janeiro', 6);
+insert into municipio (idmunicipio, nome, iduf) values (7, 'Uberlândia', 4);
+insert into municipio (idmunicipio, nome, iduf) values (8, 'Porto Alegre', 5);
+insert into municipio (idmunicipio, nome, iduf) values (9, 'União da Vitória', 2);
+
+alter table cliente drop municipio;
+alter table cliente drop uf;
+ 
+alter table cliente add idmunicipio integer;
+alter table cliente add constraint fk_cliente_idmunicipio foreign key (idmunicipio) 
+references municipio (idmunicipio);
+
+update cliente set idmunicipio = 1 where idcliente in (1, 2, 10, 11);
+update cliente set idmunicipio = 2 where idcliente in (3, 12);
+update cliente set idmunicipio = 3 where idcliente = 4;
+update cliente set idmunicipio = 4 where idcliente in (5);
+update cliente set idmunicipio = 5 where idcliente in (6, 13);
+update cliente set idmunicipio = 6 where idcliente in (7);
+update cliente set idmunicipio = 7 where idcliente in (8);
+update cliente set idmunicipio = 8 where idcliente in (9);
+update cliente set idmunicipio = 9 where idcliente in (14, 15);
+
+select * from cliente
